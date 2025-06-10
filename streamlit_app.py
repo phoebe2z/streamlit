4cb0b972b68f5d9775eca59e1dfba70a0dbd864d
Phoebe <149281825+phoebe2z@users.noreply.github.com>
2025-06-10 20:49:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,7 @@ with tabs[1]:
     with col1:
         st.image("images/real_sample.jpg", caption="Real Image (Grayscale)", use_container_width=True)
     with col2:
-        st.image("images/fake_sample.png", caption="Fake Image (Grayscale)", use_container_width=True
-)
+        st.image("images/fake_sample.png", caption="Fake Image (Grayscale)", use_container_width=True)
 
 # ---- Tab 3: Image Analysis ----
 with tabs[2]:
